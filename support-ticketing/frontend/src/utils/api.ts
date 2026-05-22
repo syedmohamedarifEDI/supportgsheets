39a456b1f21413d63fc2bf536dc8b32b29add824
syedmohamedarifEDI <syed.arif@easydataintegration.net>
2026-05-22 15:02:06 +0530

    updated backend url

--- support-ticketing/frontend/src/utils/api.ts
+++ support-ticketing/frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://backend-supportgsheets.onrender.com',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
